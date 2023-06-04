52694a73c0d7282c2fec33216a6d5365b22ab747
Yaroslav2023 <yishevchenko@ukr.net>
2023-06-04 23:30:08 +0300

    task2-complate

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe, {
+    id: 23,
+    width: 640
+});
+
+function onPlay(seconds) {
+    player.getCurrentTime().then(function(seconds) {
+        localStorage.setItem("videoplayer-current-time", seconds);
+       });};
+
+let secondSaved = localStorage.getItem("videoplayer-current-time");
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(secondSaved).then();
